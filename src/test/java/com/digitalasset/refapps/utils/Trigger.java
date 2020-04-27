46c07affcb021542f03534d7c38c7973a99ff9bf
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-04-27 18:17:14 +0200

    Implement tailing

--- src/test/java/com/digitalasset/refapps/utils/Trigger.java
+++ src/test/java/com/digitalasset/refapps/utils/Trigger.java
@@ -7,8 +7,10 @@ package com.digitalasset.refapps.utils;
 import static com.digitalasset.refapps.utils.EventuallyUtil.eventually;
 
 import com.daml.ledger.javaapi.data.Party;
+import java.io.File;
 import java.nio.file.Path;
-import java.util.Scanner;
+import org.apache.commons.io.input.Tailer;
+import org.apache.commons.io.input.TailerListenerAdapter;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +19,13 @@ public class Trigger {
   private final Logger logger = LoggerFactory.getLogger(getClass().getCanonicalName());
 
   private final ProcessBuilder processBuilder;
+  private final File logFile;
 
   private Process trigger;
 
-  private Trigger(ProcessBuilder processBuilder) {
+  private Trigger(ProcessBuilder processBuilder, File logFile) {
     this.processBuilder = processBuilder;
+    this.logFile = logFile;
   }
 
   public static Builder builder() {
@@ -30,9 +34,32 @@ public class Trigger {
 
   public void start() throws Throwable {
     logger.debug("Executing: {}", String.join(" ", processBuilder.command()));
+
+    class TriggerTailerListener extends TailerListenerAdapter {
+      private LogFinder logFinder;
+
+      TriggerTailerListener(LogFinder logFinder) {
+        this.logFinder = logFinder;
+      }
+
+      public void handle(String line) {
+        logFinder.process(line);
+      }
+    }
+
+    LogFinder logFinder = new LogFinder();
+    TriggerTailerListener listener = new TriggerTailerListener(logFinder);
+    boolean fromEndOfFile = true;
+    int delayMillis = 1000;
+    Tailer tailer = new Tailer(logFile, listener, delayMillis, fromEndOfFile);
+    Thread thread = new Thread(tailer);
+    thread.start();
+
     trigger = processBuilder.start();
-    try (Scanner scanner = new Scanner(trigger.getInputStream())) {
-      eventually(() -> Assert.assertTrue(scanner.nextLine().contains("Trigger is running")));
+    try {
+      eventually(() -> Assert.assertTrue(logFinder.found()));
+    } finally{
+      tailer.stop();
     }
     logger.info("Started.");
   }
@@ -73,8 +100,12 @@ public class Trigger {
     }
 
     public Trigger build() {
-      ProcessBuilder processBuilder = command().redirectErrorStream(true);
-      return new Trigger(processBuilder);
+      File logFile = new File(String.format("integration-test-%s.log", triggerName));
+      ProcessBuilder processBuilder =
+          command()
+              .redirectError(ProcessBuilder.Redirect.appendTo(logFile))
+              .redirectOutput(ProcessBuilder.Redirect.appendTo(logFile));
+      return new Trigger(processBuilder, logFile);
     }
 
     private ProcessBuilder command() {
