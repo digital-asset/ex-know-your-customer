db92aba8709471d28b5b383237c8c7994ebcd7df
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2021-04-06 14:26:22 +0200

    remove reviewer and QA from UI frontend-config.js

--- frontend-config.js
+++ frontend-config.js
@@ -12,28 +12,6 @@ export const version = {
 
 // --- Creating views --------------------------------------------------------------------
 
-function reviewsView() {
-    return createTab("Research review requests", "ReviewRequest@",
-        [
-            createCol("reference", "Customer name", 140, r => showKycReference(r.reference || r.observation.label)),
-            createCol("analyst", "Analyst", 80, r => r.analyst),
-            createCol("reviewer", "Reviewer", 80, r => r.reviewer),
-            createCol("time", "Time", 80, r => r.time),
-        ]
-    );
-}
-
-function qualityAssuranceView() {
-    return createTab("Quality assurance requests", "QualityAssuranceRequest@",
-        [
-            createCol("reference", "Customer name", 140, r => showKycReference(r.reference || r.observation.label)),
-            createCol("analyst", "Analyst", 80, r => r.analyst),
-            createCol("qualityAssurance", "Quality Assurance", 80, r => r.qualityAssurance),
-            createCol("time", "Time", 80, r => r.time),
-        ]
-    );
-}
-
 function publisherProvidedView(publisher) {
     return createTab("Provided KYC streams", "DataStream@",
         [
@@ -58,10 +36,6 @@ function publisherDataView(publisher, withResearchFlags) {
         createCol("time", "Time", 80, r => r.observation.time),
         createCol("observation", "Data", 160, r => showKycObservationValue(r.observation))
     ];
-    if (withResearchFlags) {
-        cols.push(createCol("reviewedBy", "Reviewed by", 80, r => r.observation.value.Research.reviewedBy));
-        cols.push(createCol("verifiedByQA", "Verified by QA", 80, r => r.observation.value.Research.verifiedByQA ? "Verified" : "Not selected"));
-    }
     return createTab("Sent KYC data", ":Publication@",
         cols,
         {
@@ -79,10 +53,6 @@ function consumerView(consumer, withResearchFlags) {
         createCol("reference", "Customer name", 140, r => showKycReference(r.observation.label)),
         createCol("observation", "Data", 160, r => showKycObservationValue(r.observation))
     ];
-    if (withResearchFlags) {
-        cols.push(createCol("reviewedBy", "Reviewed by", 80, r => r.observation.value.Research.reviewedBy));
-        cols.push(createCol("verifiedByQA", "Verified by QA", 80, r => r.observation.value.Research.verifiedByQA ? "Verified" : "Not selected"));
-    }
     return createTab("Received KYC data", ":Publication@",
         cols,
         {
@@ -346,20 +316,6 @@ export const customViews = (userId, party, role) => {
             currentTimeView
         };
     }
-    if (partyIs('KYC_QA')) {
-        return {
-            qualityAssuranceInstance,
-            currentTimeView
-        };
-    }
-
-    if (partyIs('KYC_Reviewer')) {
-        return {
-            reviewsViewInstance,
-            currentTimeView
-        };
-    }
-
     const publisherDataViewWithFlagsInstance = publisherDataView(party, true);
     const consumerViewInstance = consumerView(party, false);
     const consumerViewWithFlagsInstance = consumerView(party, true);
